5229ce62f14751e8a1e9c141fa07ce1bbaac6325
oripkgo <oripkgo@gmail.com>
2022-08-27 16:02:56 +0900

    1. 공지사항 조회 쿼리에서 회원등급별로 조회 되도록 수정

--- src/main/java/com/watcher/param/NoticeParam.java
+++ src/main/java/com/watcher/param/NoticeParam.java
@@ -6,6 +6,7 @@ import com.watcher.dto.NoticeDto;
 public class NoticeParam extends NoticeDto {
     String search_id;
     String search_keyword;
+    String search_level;
 
     public String getSearch_id() {
         return search_id;
@@ -21,5 +22,13 @@ public class NoticeParam extends NoticeDto {
 
     public void setSearch_keyword(String search_keyword) {
         this.search_keyword = search_keyword;
+    }
+
+    public String getSearch_level() {
+        return search_level;
+    }
+
+    public void setSearch_level(String search_level) {
+        this.search_level = search_level;
     }
 }
