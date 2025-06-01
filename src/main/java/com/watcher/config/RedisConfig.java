94b126c8aa18ea6047b59cf1c6376eb3e82baa7c
oripkgo <oripkgo@gmail.com>
2025-06-01 19:05:22 +0900

    fix : 메이븐 빌드 오류 해결

--- src/main/java/com/watcher/config/RedisConfig.java
+++ src/main/java/com/watcher/config/RedisConfig.java
@@ -3,10 +3,10 @@ package com.watcher.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+//import org.springframework.data.redis.connection.RedisConnectionFactory;
+//import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+//import org.springframework.data.redis.core.RedisTemplate;
+//import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 //@Configuration
 //@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
